27cd9f0661be21df1e797381280d2a46cbe35e78
KarenHeredia89 <karenherediadg@gmail.com>
2016-06-06 15:45:38 -0500

    Responsive

---
+++ js/_main.js
@@ -0,0 +1,15 @@
+$(function () {
+  // initialize skrollr if the window width is large enough
+  if ($(window).width() > 767) {
+    skrollr.init({
+      forceHeight: false
+    });
+  }
+
+  // disable skrollr if the window is resized below 768px wide
+  $(window).on('resize', function () {
+    if ($(window).width() <= 767) {
+      skrollr.init().destroy(); // skrollr.init() returns the singleton created above
+    }
+  });
+});
